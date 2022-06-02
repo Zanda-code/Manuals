f48effd435ae6068caa2a9813efab7ae50095375
Zanda-code <76736977+Zanda-code@users.noreply.github.com>
2022-06-02 15:05:07 +0200

    end first div - alarm control panel

--- script.js
+++ script.js
@@ -12,8 +12,6 @@ const newWindowFirstBtn = document.querySelector(`#newWindow__firstBtn`);
 const newWindowSecondBtn = document.querySelector(`#newWindow__secondBtn`);
 const closeBtn = document.querySelector(`.mainContent__close`);
 
-console.log(myJson[`DSC`]);
-
 const createProducer = () => {
     names.forEach((e, i) => {
         e = document.createElement(`option`);
@@ -21,53 +19,21 @@ const createProducer = () => {
         selectProducer.appendChild(e);
     })
 }
-
 const availableModel = () => {
     if (selectProducer.value === `Wybierz producenta...`){
         selectModel.setAttribute(`disabled`, `True`);
     }
     else{
         selectModel.removeAttribute(`disabled`);
+        selectModel.textContent = ``;
         const newArray = myJson[`${selectProducer.value}`];
-        const newArrayFiltered = newArray.filter(i => i.name === selectModel.value);
-        const newOption = document.createElement(`option`);
-        selectModel.appendChild(newOption);
-        selectModel.innerText = `${newArrayFiltered[0]}`;
+        newArray.forEach((el, i) => {
+        el = document.createElement(`option`);
+        selectModel.appendChild(el);
+        el.innerText = `${newArray[i].name}`;
+    }) 
     }
 }
-/*const testIndexFunction = () => {
-    const index = selectProducer.selectedIndex;
-    if(index === 0){
-        selectModel.setAttribute(`disabled`, `true`);
-    }
-    else if(index === 1){
-        selectModel.innerHTML = ``;
-        selectModel.removeAttribute(`disabled`);
-        myJson.dsc.forEach((e, i) => {
-            e = document.createElement(`option`);
-            e.innerText = `${myJson.dsc[i].name}`;
-            selectModel.appendChild(e);
-        });
-    }
-    else if(index === 2){
-        selectModel.innerHTML = ``;
-        selectModel.removeAttribute(`disabled`);
-        myJson.satel.forEach((e, i) => {
-            e = document.createElement(`option`);
-            e.innerText = `${myJson.satel[i].name}`;
-            selectModel.appendChild(e);
-        });
-    }
-    else if(index === 3){
-        selectModel.innerHTML = ``;
-        selectModel.removeAttribute(`disabled`);
-        myJson.jablotron.forEach((e, i) => {
-            e = document.createElement(`option`);
-            e.innerText = `${myJson.jablotron[i].name}`;
-            selectModel.appendChild(e);
-        });
-    }
-}*/
 const findGoodManuals = () => {
     const newArray = myJson[`${selectProducer.value}`];
     const newArrayFiltered = newArray.filter(i => i.name === selectModel.value);
@@ -83,7 +49,6 @@ const findGoodManuals = () => {
 const closeWindow = () => {
     newWindow.style.display = `none`;
 }
-
 const events = () => {
     selectProducer.addEventListener(`change`, availableModel);
     btn.addEventListener(`click`, findGoodManuals);
